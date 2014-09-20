9cc4475f21825c3e24fdeef8d640b32ab2123026
Helen Zhi Huang <zhihuahuang@hotmail.com>
2014-09-20 09:07:11 -0500

    Submissiontest3
    test run3 add stuffs

--- testsubmission.R
+++ testsubmission.R
@@ -12,3 +12,13 @@ solve(x)
 ## new new code
 x<- matrix(1:4, 2,2)
 solve(x)
+
+
+
+## new code-more testing 3
+x<- matrix(1:4, 2,2)
+solve(x)
+
+## new new code--run 3 for testing
+x<- matrix(1:4, 2,2)
+solve(x)
