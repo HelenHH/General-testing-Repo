d912604131a3c602eedcc739e42bd634ffd2bf67
Helen Zhi Huang <zhihuahuang@hotmail.com>
2014-11-22 09:29:31 -0600

    addfolder
    add folder

---
+++ addfoldertest/shiny testing.R
@@ -0,0 +1,28 @@
+shinyUI(navbarPage(textOutput("about the app"),
+                   tabPanel("About",
+                            includeMarkdown("README.md"))))
+shinyUI(pageWithSidebar(
+        headerPanel("Illustrating inputs"),
+        sidebarPanel(
+                numericInput('id1', 'Numeric input, labeled id1', 0, min = 0, max = 10, step = 1),
+                checkboxGroupInput("id2", "Checkbox",
+                                   c("Value 1" = "1",
+                                     "Value 2" = "2",
+                                     "Value 3" = "3")),
+                dateInput("date", "Date:")
+        ),
+        mainPanel(
+                h3('Illustrating outputs'),
+                h4('You entered'),
+                verbatimTextOutput("oid1"),
+                h4('You entered'),
+                verbatimTextOutput("oid2"),
+                h4('You entered'),
+                verbatimTextOutput("odate")
+        )
+))
+
+shinyUI(navbarPage(textOutput("about the app"),
+                   tabPanel("About",
+                            includeMarkdown("README.md"))))
+
--- testsubmission.R
+++ testsubmission.R
@@ -22,3 +22,7 @@ solve(x)
 ## new new code--run 3 for testing
 x<- matrix(1:4, 2,2)
 solve(x)
+
+
+
+### HSA number 9cc4475f21825c3e24fdeef8d640b32ab2123026
